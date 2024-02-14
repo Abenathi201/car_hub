dd57e3f016c61eebd98e5e4fa6454cab60daaba5
Abenathi201 <abenathi.sindaphi201@gmail.com>
2024-02-14 15:59:38 +0200

    car showcases

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { NavBar, Footer } from "@/components";
+
+export const metadata: Metadata = {
+  title: "Car Hub",
+  description: "Discover the best cars in the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
+}

953b9895aaf2dee222aa7c9d7a8f52730e712dca
Abenathi201 <abenathi.sindaphi201@gmail.com>
2024-02-15 13:24:24 +0200

    done fetching products

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 
-import { Hero, SearchBar, CustomFilter } from "@/components";
+import { fetchCars } from "@/utils";
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -21,6 +26,19 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => <CarCard car={car} />)}
+            </div>
+          </section>
+        ): (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
